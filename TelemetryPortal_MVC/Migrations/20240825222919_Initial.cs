ca421df8830c689c5e32ddd6950b84c17eb913ab
K G Moota <41505212@mynwu.ac.za>
2024-08-26 00:46:12 +0200

    Update database and test the application.

---
+++ TelemetryPortal_MVC/Migrations/20240825222919_Initial.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TelemetryPortal_MVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "Config");
+
+            migrationBuilder.CreateTable(
+                name: "Client",
+                schema: "Config",
+                columns: table => new
+                {
+                    ClientID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ClientName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PrimaryContactEmail = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateOnboarded = table.Column<DateTime>(type: "datetime", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Client", x => x.ClientID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Project",
+                schema: "Config",
+                columns: table => new
+                {
+                    ProjectID = table.Column<Guid>(type: "uniqueidentifier", nullable: false, defaultValueSql: "(newid())"),
+                    ProjectName = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: true),
+                    ProjectDescription = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: true),
+                    ProjectCreationDate = table.Column<DateTime>(type: "datetime", nullable: true, defaultValueSql: "(dateadd(hour,(2),getdate()))"),
+                    ProjectStatus = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: true),
+                    ClientID = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Project", x => x.ProjectID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Client",
+                schema: "Config");
+
+            migrationBuilder.DropTable(
+                name: "Project",
+                schema: "Config");
+        }
+    }
+}
